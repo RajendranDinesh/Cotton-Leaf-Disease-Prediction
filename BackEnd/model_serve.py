1670134ceba30b448849603b94b20ccd787c3bfa
RajendranDinesh <dinesh2842004@gmail.com>
2023-06-25 21:12:42 +0530

    first commit

---
+++ BackEnd/model_serve.py
@@ -0,0 +1,91 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+import tempfile
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+temp_dir = "" # Path to a temporary directory
+
+# Load your trained model
+model = tf.keras.models.load_model('') # Path to your trained model
+
+# Define a dictionary to map class indices to labels
+class_labels = {
+    0: 'Alternaria',
+    1: 'Bacterial Blight',
+    2: 'Grey Mildew',
+    3: 'Healthy',
+}
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the image data from the request
+    image_data = request.files['image']
+
+    # Save the uploaded file to a temporary location
+    with tempfile.NamedTemporaryFile(suffix='.jpg', dir=temp_dir, delete=False) as temp_file:
+        image_data.save(temp_file.name)
+
+        # Read and preprocess the image
+        img = image.load_img(temp_file.name, target_size=(224, 224))
+        img_array = image.img_to_array(img)
+        preprocessed_data = preprocess_input(np.expand_dims(img_array, axis=0))
+
+        # Perform the prediction
+        prediction = model.predict(preprocessed_data)
+
+        # Get the predicted class index
+        predicted_class_index = np.argmax(prediction)
+
+        # Get the predicted class label
+        predicted_class_label = class_labels[predicted_class_index]
+
+        # Create a response dictionary
+        response = {'prediction': predicted_class_label}
+
+    # Return the response as JSON
+    return jsonify(response)
+
+
+
+# Load the leaf classification model
+leaf_model = tf.keras.models.load_model('') # Path to your leaf classification model
+
+# Define class labels
+class_labels_classify = {
+    0: 'Cotton Leaf',
+    1: 'Other Leaf'
+}
+
+@app.route('/classify', methods=['POST'])
+def classify_leaf():
+    # Get the image data from the request
+    image_data = request.files['image']
+
+    # Save the uploaded file to a temporary location
+    with tempfile.NamedTemporaryFile(suffix='.jpg', dir=temp_dir, delete=False) as temp_file:
+        image_data.save(temp_file.name)
+
+        # Read and preprocess the image
+        img = image.load_img(temp_file.name, target_size=(224, 224))
+        img_array = image.img_to_array(img)
+        preprocessed_data = preprocess_input(np.expand_dims(img_array, axis=0))
+
+        # Perform leaf classification
+        prediction = leaf_model.predict(preprocessed_data)
+        predicted_class_index = np.argmax(prediction)
+        predicted_class_label = class_labels_classify[predicted_class_index]
+
+        # Create a response dictionary
+        response = {'classification': predicted_class_label}
+
+    # Return the response as JSON
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ FrontEnd/src/App.js
@@ -0,0 +1,164 @@
+import React, { useState } from 'react';
+import './App.css';
+import Dropzone from 'react-dropzone';
+import { Loader } from './loader/Loader';
+import { DropContainer } from './DropContainer';
+import { Done } from './loader/Checker';
+import { Cross } from './loader/Error'
+import axios from 'axios';
+
+const Modal = ({ isOpen, onClose, children, clear }) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        {children}
+        <button className="modal-close" onClick={onClose}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+function App() {
+
+  const [image, setImage] = useState(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [disease, setDisease] = useState(null);
+
+  const [isLeafCheckComplete, setIsLeafCheckComplete] = useState(false);
+
+  const [isCottonLeaf, setIsCottonLeaf] = useState(false);
+
+  const [isPredicting, setIsPredicting] = useState(false);
+
+  const [isPredictionCompleted, setIsPredictionCompleted] = useState(false);
+
+  const [showLeafDialog, setShowLeafDialog] = useState();
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setImage('');
+    setIsCottonLeaf(false);
+    setIsLeafCheckComplete(false);
+    setIsPredicting(false);
+    setIsPredictionCompleted(false);
+    setShowLeafDialog();
+    setIsOpen(false);
+  };
+
+  const handleFileDrop = async (acceptedFiles, rejectedFiles) => {
+
+    const formData = new FormData();
+    const imageFiles = acceptedFiles.filter(file => file.type.startsWith('image/'));
+
+    setImage(imageFiles[0]);
+    if (imageFiles.length > 0) {
+      formData.append('image', imageFiles[0]);
+      openModal();
+      try{
+        await axios.post("http://127.0.0.1:5000/classify", formData, {
+          headers:{
+            'Content-Type' : 'multipart/form-data',
+          },
+        }).then(async (response) => {
+          setIsLeafCheckComplete(true);
+
+          if(response.data.classification === "Cotton Leaf"){
+            setIsCottonLeaf(true);
+            setIsPredicting(true);
+            try {
+              await axios.post("http://127.0.0.1:5000/predict", formData, {
+                headers:{
+                  'Content-Type' : 'multipart/form-data',
+                },
+              }).then((response) => {
+                setIsPredictionCompleted(true);
+                setDisease(response.data.prediction);
+              })
+            } catch (error) {
+              console.log(error)
+            }
+          }
+          else{
+            setShowLeafDialog(true);
+          }
+        });
+      }catch (error){
+        console.log(error)
+      }
+
+    } else {
+      alert('Only image files are allowed!');
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="left">
+        <div className="wrapper">
+          <h1>COTTON</h1>
+          <h1>LEAF</h1>
+          <h1>DISEASE</h1>
+          <h1>PREDICTION</h1>
+        </div>
+      </div>
+      <div className='right'>
+        <div className='upload-container'>
+          <div class="container">
+            <div class="box">
+              <Dropzone onDrop={handleFileDrop} multiple={false}>
+                  {
+                    ({getRootProps, getInputProps}) => (
+                      <div {...getRootProps()}>
+                          <input {...getInputProps()} accept="image/*"></input>
+                          <DropContainer/>
+                      </div>
+                    )
+                  }
+              </Dropzone>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Modal isOpen={isOpen} onClose={closeModal}>
+          <div className='aline'>
+            <h4>Checking for Leaf Type</h4>
+            {isLeafCheckComplete? (
+              isCottonLeaf? (<Done/>) : (<Cross/>)
+            ) : (<Loader/>)}
+          </div>
+          {isPredicting ? (
+          <div className='aline'>
+          <h4>Prediction</h4>
+            {isPredictionCompleted? (
+              <>
+                <>{disease}</>
+                <img src={URL.createObjectURL(image)} height={145} width={145} alt='Retina'/>
+              </>
+            ):(
+              <Loader/>
+            )}
+          </div>) : (<></>)}
+          {showLeafDialog?
+          (<div className='aline'>
+            <p>
+              Please Upload a Picture Of a Cotton Leaf..
+            </p>
+          </div>) : (<></>)
+          }
+      </Modal>
+    </div>
+  );
+}
+
+export default App;
